2d3a3250fbe9257c4636a4eb0ba35b698e356d70
ashtneoi <ashtneoi@gmail.com>
2018-04-08 15:49:14 -0700

    Parse file; refactor key parser

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 extern crate toml;
 
 use std::env;
+use std::fs;
+use std::io;
 use std::process::exit;
+use std::str::FromStr;
 
 use toml::Value;
 
@@ -22,25 +25,16 @@ fn exit_with_usage() -> ! {
     exit(2);
 }
 
-fn main() {
-    let arg_strings: Vec<_> = env::args().collect();
-    let args = arg_strings.as_strs();
-    if args.len() != 3 {
-        exit_with_usage();
-    }
-
-    let filename = args[1];
-    let key_str = format!(
-        "[{}]\n{} = 0", args[2], SENTINEL_KEY);
-
-    let fake_table = match key_str.parse::<Value>().unwrap() {
+fn parse_key(key_str: &str) -> Option<Vec<String>> {
+    let fake_table_string = format!(
+        "[{}]\n{} = 0", key_str, SENTINEL_KEY);
+    let fake_table = match fake_table_string.parse::<Value>().ok()? {
         Value::Table(x) => x,
         _ => panic!(),
     };
 
     let mut keys: Vec<String> = Vec::new();
     let mut t = &fake_table;
-
     loop {
         let (key, val) = t.iter().next().unwrap();
         if key == SENTINEL_KEY {
@@ -53,5 +47,36 @@ fn main() {
         };
     }
 
-    println!("{:?}", keys);
+    Some(keys)
+}
+
+fn main() {
+    let arg_strings: Vec<_> = env::args().collect();
+    let args = arg_strings.as_strs();
+    if args.len() != 3 {
+        exit_with_usage();
+    }
+
+    let filename = args[1];
+    let key = parse_key(args[2]).unwrap_or_else(|| {
+        eprintln!("error: invalid key");
+        exit(2);
+    });
+
+    let file = match fs::read(filename) {
+        Ok(x) => x,
+        Err(e) => {
+            eprintln!("error: can't read file ({})", e);
+            exit(2);
+        },
+    };
+    let parsed: Value = match toml::de::from_slice(&file) {
+        Ok(x) => x,
+        Err(e) => {
+            eprintln!("error: can't parse file ({})", e);
+            exit(1);
+        },
+    };
+
+    println!("{:?}", parsed);
 }
